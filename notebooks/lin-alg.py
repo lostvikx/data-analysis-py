b9140d80c9c3fd5d99fe65fd37ba1c085b5871bf
Vikram Negi <viknegi0@gmail.com>
2022-06-19 18:46:16 +0530

    basic linalg

--- notebooks/lin-alg.py
+++ notebooks/lin-alg.py
@@ -5,11 +5,56 @@
 import numpy as np
 
 # %%
-a = np.random.standard_normal((2, 3))
-b = np.random.standard_normal((3, 2))
+def rand_arr(shape=5, times=1):
+  return np.random.standard_normal(shape) * times
+
+# %%
+a = rand_arr((2, 3))
+b = rand_arr((3, 2))
+
+# %% [markdown]
+# ![Dot product](imgs/dot-product-matrix.jpg)
 
 # %%
 np.dot(a, b)
+# Or
+a.dot(b)
 
 # %%
+a * np.ones(3)
 
+# %%
+c=rand_arr((2,2))
+d=rand_arr((2,2))
+
+# %% [markdown]
+# **Note**: Element-wise `*` multiplication is not equal `dot` product of two matrices:
+
+# %%
+print(c*d == c.dot(d))
+
+# %%
+x = np.ceil(rand_arr((3, 3)))
+y = np.ceil(rand_arr((3, 3)))
+
+# %% [markdown]
+# ![How to inverse a matrix](imgs/inverse-matrix.jpg)
+
+# %%
+mat_x = x.T @ x
+inv_x = np.linalg.inv(mat_x)
+
+mat_x @ inv_x
+
+# %%
+y.dot(y.T) == y.T.dot(y)
+
+# %%
+[fn for fn in dir(np.linalg) if fn[0] != "_"]
+
+# %%
+help(np.linalg.inv)
+
+# %%
+# sum of the diagnal of a matrix
+np.trace(y)
