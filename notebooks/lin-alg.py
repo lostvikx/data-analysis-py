5052c359a7e58a68d55f2feb5e8017ba2687bead
Vikram Negi <viknegi0@gmail.com>
2022-06-19 14:35:57 +0530

    linarg init

---
+++ notebooks/lin-alg.py
@@ -0,0 +1,15 @@
+# %% [markdown]
+# # Linear Algebra
+
+# %%
+import numpy as np
+
+# %%
+a = np.random.standard_normal((2, 3))
+b = np.random.standard_normal((3, 2))
+
+# %%
+np.dot(a, b)
+
+# %%
+
