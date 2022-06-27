1ce5b1734a6672edb1d7a9bc0641c3b3a350d8b8
Vikram Negi <viknegi0@gmail.com>
2022-06-27 19:11:17 +0530

    rm data/

--- notebooks/accessing-data.py
+++ notebooks/accessing-data.py
@@ -9,7 +9,7 @@ import pandas as pd
 import numpy as np
 
 # %%
-ex1 = "data/examples/ex1.csv"
+ex1 = "examples/ex1.csv"
 
 # %% [bash]
 # ! cat $ex1
@@ -24,26 +24,26 @@ test_df1
 # %%
 # Creates column index [0:)
 df_col_names = ["a", "b", "c", "d", "message"]
-test_df2 = pd.read_csv("data/examples/ex2.csv", header=None, names=df_col_names)
+test_df2 = pd.read_csv("examples/ex2.csv", header=None, names=df_col_names)
 test_df2
 
 # %% [markdown]
 # Let's say we want the "message" column as the index
 
 # %%
-pd.read_csv("data/examples/ex2.csv", names=df_col_names, index_col="message")
+pd.read_csv("examples/ex2.csv", names=df_col_names, index_col="message")
 
 # %% [markdown]
 # Hierarchical Index:
 
 # %%
-pd.read_csv("data/examples/csv_mindex.csv", index_col=["key1", "key2"])
+pd.read_csv("examples/csv_mindex.csv", index_col=["key1", "key2"])
 
 # %% [markdown]
 # Some times a table might not have a fixed or use some other delimiter, we can still parse that data:
 
 # %%
-pd.read_csv("data/examples/ex3.txt", sep="\s+")
+pd.read_csv("examples/ex3.txt", sep="\s+")
 
 # %% [markdown]
 # The `sep` argument can take a regular expression as well as a string.
@@ -52,26 +52,26 @@ pd.read_csv("data/examples/ex3.txt", sep="\s+")
 # Because there is one fewer column name (first row) than the columns (all other rows), pandas infers the first column as the index.
 
 # %%
-pd.read_csv("data/examples/ex4.csv", skiprows=[0,2,3])
+pd.read_csv("examples/ex4.csv", skiprows=[0,2,3])
 
 # %% [markdown]
 # Handling missing data while reading a file is important. By default, `pandas` parses blank space, or *sentinels* such as NA & NULL.
 
 # %%
 # !cat "data/examples/ex5.csv"
-pd.read_csv("data/examples/ex5.csv")
+pd.read_csv("examples/ex5.csv")
 
 # %%
-pd.read_csv("data/examples/ex5.csv", na_values=["foo"])
+pd.read_csv("examples/ex5.csv", na_values=["foo"])
 
 # %% [markdown]
 # We can add more sentinels by using the `na_values` argument.
 
 # %%
-pd.read_csv("data/examples/ex5.csv", keep_default_na=False)
+pd.read_csv("examples/ex5.csv", keep_default_na=False)
 
 # %%
-df5 = pd.read_csv("data/examples/ex5.csv", keep_default_na=False, na_values=["NA", "null", "", "NULL"])
+df5 = pd.read_csv("examples/ex5.csv", keep_default_na=False, na_values=["NA", "null", "", "NULL"])
 df5
 
 # %%
@@ -85,7 +85,7 @@ sentinels = {
   "something": ["two"], 
   "message": ["foo"]
 }
-pd.read_csv("data/examples/ex5.csv", na_values=sentinels)
+pd.read_csv("examples/ex5.csv", na_values=sentinels)
 
 # %%
 
